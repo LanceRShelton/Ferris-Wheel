3c579459d62966766a05db169f20486603a24898
LanceRShelton <74195277+LanceRShelton@users.noreply.github.com>
2022-09-15 07:54:40 -0700

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    int sum;
+    cout << "put in the first number";
+    cin >> a;
+    cout << "put in the second number";
+    cin >> b;
+    cout << a+b = sum;
+    return 0;
+}
